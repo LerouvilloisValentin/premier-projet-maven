ea83830133e2d0be59c911e1c2e8fe9a1e576099
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2024-04-23 11:22:22 +0200

    tp3 maven done

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,6 +1,8 @@
 package dev;
 
-//import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
+
+import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
@@ -8,7 +10,11 @@ package dev;
  */
 public class App {
 	public static void main(String[] args) {
-//		String asciiArt = FigletFont.convertOneLine("App");
-//		System.out.println(asciiArt);
+		  String titre = ResourceBundle.getBundle("application").getString("titre"); 
+		  String asciiArt = FigletFont.convertOneLine(titre);
+		  System.out.println(asciiArt);
+		  String environnement = ResourceBundle.getBundle("application").getString(
+				  "environnement");
+				    System.out.println("Environnement : " + environnement);
 	}
 }
